8e188ede84f75647890ee857db55e8a3fa8e70fb
Dannybros <akbaromega89@gmail.com>
2023-06-12 09:33:01 +0700

    replaced old db

--- DB/hotel_management.sql
+++ DB/hotel_management.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 03, 2023 at 02:33 PM
+-- Generation Time: Jun 12, 2023 at 04:23 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -30,9 +30,10 @@ SET time_zone = "+00:00";
 CREATE TABLE `booking` (
   `booking_id` varchar(30) NOT NULL,
   `customer_id` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`customer_id`)),
-  `date_check_in` date NOT NULL,
-  `date_check_out` date NOT NULL,
+  `date_in` date NOT NULL,
+  `date_out` date NOT NULL,
   `duration` int(11) NOT NULL,
+  `booking_status` varchar(20) DEFAULT NULL,
   `total_payment` int(11) NOT NULL,
   `payment_status` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -41,37 +42,9 @@ CREATE TABLE `booking` (
 -- Dumping data for table `booking`
 --
 
-INSERT INTO `booking` (`booking_id`, `customer_id`, `date_check_in`, `date_check_out`, `duration`, `total_payment`, `payment_status`) VALUES
-('b646ef0cdddace', '[\"c64589f6408656\"]', '2023-05-26', '2023-05-28', 2, 1000000, 'true'),
-('b6474ac25b59ec', '[\"c645887aa707ad\",\"c6458aa2b43ead\"]', '2023-05-29', '2023-06-01', 3, 2400000, 'true');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `booking_log`
---
-
-CREATE TABLE `booking_log` (
-  `booking_log_id` int(11) NOT NULL,
-  `booking_id` varchar(20) NOT NULL,
-  `room_id` varchar(20) NOT NULL,
-  `time` date DEFAULT NULL,
-  `booking_status` varchar(20) NOT NULL,
-  `memo` varchar(200) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `booking_log`
---
-
-INSERT INTO `booking_log` (`booking_log_id`, `booking_id`, `room_id`, `time`, `booking_status`, `memo`) VALUES
-(1, 'b646ef0cdddace', 'r645f947e1df75', '2023-05-25', 'Reserved', ''),
-(2, 'b646ef0cdddace', 'r645f947e1df75', '2023-05-26', 'Checked In', ''),
-(4, 'b646ef0cdddace', 'r645f9fcb6ad67', '2023-05-27', 'Moved', ''),
-(7, 'b646ef0cdddace', 'r645f9fcb6ad67', '2023-05-28', 'Checked Out', ''),
-(8, 'b6474ac25b59ec', 'r645fa0361833c', '2023-05-29', 'Reserved', ''),
-(9, 'b6474ac25b59ec', 'r645fa0361833c', '2023-05-29', 'Checked In', ''),
-(10, 'b6474ac25b59ec', 'r645fa0361833c', '2023-06-01', 'Checked Out', '');
+INSERT INTO `booking` (`booking_id`, `customer_id`, `date_in`, `date_out`, `duration`, `booking_status`, `total_payment`, `payment_status`) VALUES
+('b6485beb5c3810', '[\"c64589f6408656\"]', '2023-06-13', '2023-06-16', 4, 'Confirmed', 4000000, 'true'),
+('b6485df41ac4c9', '[\"c64639473b3a55\"]', '2023-06-11', '2023-06-13', 3, 'Staying', 2400000, 'true');
 
 -- --------------------------------------------------------
 
@@ -94,7 +67,7 @@ CREATE TABLE `customer` (
 --
 
 INSERT INTO `customer` (`cust_id`, `cust_name`, `cust_firstname`, `cust_bd`, `contact`, `email`, `passport`) VALUES
-('c645887aa707ad', 'Testla', 'YOUR', '', '1232', 'tese@ds', 'tes123'),
+('c645887aa707ad', 'Testla', 'YOUR', '2001-06-07', '1232', 'tese@ds', 'tes123'),
 ('c64589f6408656', 'Holy', 'FUCK', '2023-05-06', '2332', 'eew@hb', '324324'),
 ('c6458a8283bd3d', 'as232', 'hhh', '', '2355323', 'sdffd@fgs', '234fs3522'),
 ('c6458aa2b43ead', 'DD', 'ff', '2023-05-03', '43234', 'dsd@ge', '323fds22'),
@@ -123,12 +96,37 @@ CREATE TABLE `room` (
 INSERT INTO `room` (`room_id`, `room_name`, `room_type_id`, `room_status`, `booking_id`) VALUES
 ('r645f947e1df75', 'A-101', 'rt645e45c73d72b', 'Free', NULL),
 ('r645f9fcb6ad67', 'A-102', 'rt645e45c73d72b', 'Free', NULL),
-('r645fa0361833c', 'A-103', 'rt645e45e74e5a9', 'Free', NULL),
+('r645fa0361833c', 'A-103', 'rt645e45e74e5a9', 'Occupied', 'b6485df41ac4c9'),
 ('r645fa058cf1e2', 'A-104', 'rt645e45f9a305c', 'Free', NULL),
-('r645fa1962c973', 'A-105', 'rt645e46045d1e2', 'Free', NULL),
+('r645fa1962c973', 'A-105', 'rt645e46045d1e2', 'Booked', 'b6485beb5c3810'),
 ('r645fa1ac8cc62', 'B-101', 'rt645e45c73d72b', 'Free', NULL),
 ('r645fa41e4d89b', 'B-102', 'rt645e45e74e5a9', 'Free', NULL),
-('r645fa43bc824f', 'B-103', 'rt645e45e74e5a9', 'Free', NULL);
+('r6485b71182519', 'B-103', 'rt645e45f9a305c', 'Free', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `room_log`
+--
+
+CREATE TABLE `room_log` (
+  `room_log_id` int(11) NOT NULL,
+  `booking_id` varchar(20) NOT NULL,
+  `room_id` varchar(20) NOT NULL,
+  `old_room_id` varchar(20) DEFAULT NULL,
+  `time` date DEFAULT NULL,
+  `action` varchar(20) NOT NULL,
+  `memo` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `room_log`
+--
+
+INSERT INTO `room_log` (`room_log_id`, `booking_id`, `room_id`, `old_room_id`, `time`, `action`, `memo`) VALUES
+(65, 'b6485beb5c3810', 'r645fa058cf1e2', NULL, '2023-06-11', 'Reserved', ''),
+(66, 'b6485df41ac4c9', 'r645fa0361833c', NULL, '2023-06-11', 'Reserved', ''),
+(68, 'b6485df41ac4c9', 'r645fa0361833c', NULL, '2023-06-11', 'Checked In', '');
 
 -- --------------------------------------------------------
 
@@ -163,12 +161,6 @@ ALTER TABLE `booking`
   ADD PRIMARY KEY (`booking_id`);
 
 --
--- Indexes for table `booking_log`
---
-ALTER TABLE `booking_log`
-  ADD PRIMARY KEY (`booking_log_id`);
-
---
 -- Indexes for table `customer`
 --
 ALTER TABLE `customer`
@@ -181,6 +173,12 @@ ALTER TABLE `room`
   ADD PRIMARY KEY (`room_id`);
 
 --
+-- Indexes for table `room_log`
+--
+ALTER TABLE `room_log`
+  ADD PRIMARY KEY (`room_log_id`);
+
+--
 -- Indexes for table `room_type`
 --
 ALTER TABLE `room_type`
@@ -191,10 +189,10 @@ ALTER TABLE `room_type`
 --
 
 --
--- AUTO_INCREMENT for table `booking_log`
+-- AUTO_INCREMENT for table `room_log`
 --
-ALTER TABLE `booking_log`
-  MODIFY `booking_log_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+ALTER TABLE `room_log`
+  MODIFY `room_log_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
